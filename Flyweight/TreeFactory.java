f3230b01909fdf019015e8780b1b11a560997b23
abbasmasood9276 <145642179+abbasmasood9276@users.noreply.github.com>
2023-12-21 16:43:59 +0500

    update

---
+++ Flyweight/TreeFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Flyweight;
+
+/**
+ *
+ * @author aabik
+ */
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TreeFactory {
+    private static Map<String, TreeType> treeTypes = new HashMap<>();
+
+    public static TreeType getTreeType(String name, Color color, String otherTreeData,
+                                       boolean isFruitTree, int countOfFruits) {
+        
+        String key = name + color.getRGB();
+
+    
+        TreeType result = treeTypes.get(key);
+
+        if (result == null) {
+           
+            result = new TreeType(name, color, otherTreeData, isFruitTree, countOfFruits);
+            treeTypes.put(key, result);
+        }
+
+        return result;
+    }
+}
+
+
